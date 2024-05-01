5486bfd332a3bfd0a7ba7d428287062f2b37ebaa
zulfiqarshafar <zul.shafar@gmail.com>
2024-05-01 23:00:54 +0800

    feat: update url and static settings

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
+app_name = "main"
+
 urlpatterns = [
-    path('', views.home, name="home")
-]
+    path('', views.HomeView.as_view(), name='home'),
+    path('detail/<int:id>/', views.DetailView.as_view(), name='detail'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
